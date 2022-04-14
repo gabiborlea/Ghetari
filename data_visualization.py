809b6da74b2db61e5435f64f5164515a1cbc8eb6
Gabriel Borlea <gabriel.borlea@8x8.com>
2022-04-14 18:56:02 +0300

    add plots

--- data_visualization.py
+++ data_visualization.py
@@ -2,7 +2,7 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
-file = open("resources/Environment_Temperature_change_E_All_Data_NOFLAG.csv")
+file = open("resources/Environment_Temperature_change_E_All_Data_NOFLAG.csv", encoding = "ISO-8859-1")
 csvreader = csv.reader(file)
 header = []
 header = next(csvreader)
@@ -21,16 +21,21 @@ years = np.arange(1961, 2020, 1)
 month_idx = np.arange(1, len(afghanistan)+1, 1)
 
 # plot data for each month
-for i in range(len(afghanistan)):
-    plt.plot(years, afghanistan[i], marker='o')
-    plt.title("Afghanistan - temperature change for month " + month_names[i])
-    plt.yticks([])
-    plt.show()
+# for i in range(len(afghanistan)):
+#     plt.plot(years, afghanistan[i], marker='o')
+#     plt.title("Afghanistan - temperature change for month " + month_names[i])
+#     plt.yticks([])
+#     plt.show()
+#
+# # # average data to have one value for each month
+# avg_month = np.average(list(afghanistan), axis=1)
+# plt.plot(month_idx, avg_month, marker='o')
+# plt.title("Average temperature change for months")
+# plt.xticks(month_idx)
+# plt.show()
 
-# average data to have one value for each month
-avg_month = np.average(list(afghanistan), axis=1)
-plt.plot(month_idx, avg_month, marker='o')
-plt.title("Average temperature change for months")
-plt.xticks(month_idx)
+avg = np.average(list(afghanistan), axis=0)
+plt.plot(years, avg, marker='o')
+plt.title("Average temperature change for years")
 plt.show()
 
